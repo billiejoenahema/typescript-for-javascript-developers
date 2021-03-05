8c09d3e09ef9d81b7268f4d1720ec667ae9f4ee4
billiejoenahema <billiejoenahema@gmail.com>
2021-03-05 18:29:33 +0900

    create: as-const.ts

---
+++ src/as-const.ts
@@ -0,0 +1,17 @@
+export { }
+
+let name = 'Billie'
+
+name = 'Joe'
+
+// 実質的に定数扱いになる
+let nickname = 'Bill' as const
+nickname = 'Bill'
+
+let profile = {
+    name: 'Billie',
+    height: 173
+} as const
+
+profile.name = 'Bull'
+profile.height = 180
