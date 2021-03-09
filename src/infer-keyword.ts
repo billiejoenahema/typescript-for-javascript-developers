a66a2764459c7b87ea9cd365116da898611aa690
billiejoenahema <billiejoenahema@gmail.com>
2021-03-09 21:03:08 +0900

    create: infer-keyword.ts

--- src/infer-keyword.ts
+++ src/infer-keyword.ts
@@ -7,3 +7,5 @@ function add(a: number, b: number) {
 console.log(add(1, 2))
 
 type ReturnTypeFromAdd = ReturnType<typeof add>
+
+type MyReturnType<T extends (...args: any) => any> = T extends (...args: any) => infer R ? R : any;
